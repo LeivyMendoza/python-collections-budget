ff3656e9d7b89c32f54c1f277fa2920f3373ae52
Leivy Mendoza <mendozaleivy@gmail.com>
2023-02-10 12:02:49 -0500

    fix bug with the main function

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,5 +1,6 @@
 from . import Expense
 
+
 class BudgetList():
     def __init__(self, budget):
         self.budget = budget
@@ -14,18 +15,20 @@ class BudgetList():
             self.sum_expenses += item
         else:
             self.overages.append(item)
-            self.overages += item
+            self.sum_overages += item
 
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
-    def main(self):
-        myBudgetList = BudgetList(1200)
-        expenses = Expense.Expenses()
-        expenses.read_expenses('data/spending_data.csv')
-        for expense in expenses.list:
-            myBudgetList.append(expense.amount)
-        print('The count of all expenses: ' + str(len(myBudgetList)))
 
-    if __name__ == "__main__":
-        main()
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+
+if __name__ == "__main__":
+    main()
